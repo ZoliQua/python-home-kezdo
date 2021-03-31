8e32f3e83a469fe60656cf6cc2bd4111b2044994
Zoltan Dul <zoltan.dul@gmail.com>
2021-03-31 23:51:15 +0100

    update otos-lotto files

--- test-keno-create.py
+++ test-keno-create.py
@@ -9,24 +9,27 @@
 #
 # Written by Zoltan Dul (2021)
 #
-from typing import Dict
 
 import csv
 import random
-import string
 
 #
 # Creating a random Keno
 #
 
+# Adding spaceholder text for the beginning of the file
 line_starter = ";99;9;2022.12.31."
 lines = ["Year;Week;Day;Date;Nr1;Nr2;Nr3;Nr4;Nr5;Nr6;Nr7;Nr8;Nr9;Nr10;Nr11;Nr12;Nr13;Nr14;Nr15;Nr16;Nr17;Nr18;Nr19;Nr20"]
+
+# Random cycles for create numbers
 
 for i in range(0,8503):
 	line_num = i + 1000
 	nrs = []
+	# Creating random 20 numbers
 	while len(nrs) < 20:
 		rnd = str(random.randrange(1, 81)).zfill(2)
+		# Until we find 20 unique numbers between 1 - 80
 		if rnd not in nrs:
 			nrs.append(rnd)
 
