17e5208c08a574fd39aef8180c9e54e60ec5b1b4
Zoltan Dul <zoltan.dul@gmail.com>
2021-04-19 22:13:29 +0200

    ▶️ animate a circle in test-game-03-dino-jatek.py

--- test-game-03-dino-jatek.py
+++ test-game-03-dino-jatek.py
@@ -1,12 +1,18 @@
 from p5 import *
 
+f = None
 kaktusz = None
 dino = None
+x = 0
+y_circle = 72
+x_circle = 72
 
 def setup():
 	global kaktusz
 	global dino
+	global f
 
+	f = create_font("Arial.ttf", 10)
 	size(400, 400)
 	line(1, 3,100,200)
 	kaktusz = load_image("images/pngImageDir/jatek-kaktusz.png")
@@ -25,15 +31,61 @@ def setup():
 
 
 def draw():
-	background(255)
-	no_stroke()
-	image(kaktusz, 100, 200)
-	image(dino, 200, 300)
+	global x
+	global y_circle
+	global x_circle
+	global f
 
-	# # bright red
-	# fill(255, 0, 0)
-	# circle((72, 72), 58)
-	#
+	stroke(255, 153, 0)
+
+	background(255)
+	# no_stroke()
+
+	if x < 400:
+		x += 1
+	else:
+		x = -50
+
+	image(kaktusz, x, 200)
+	image(dino, x, 300)
+
+	if 50 < x_circle < 350 and y_circle < 350:
+		x_circle += 3
+	else:
+		if y_circle < 350 and x_circle > 350:
+			y_circle += 3
+		else:
+			if x_circle > 50:
+				x_circle -= 3
+			else:
+				y_circle -= 3
+				if y_circle < 72:
+					x_circle = 51
+					y_circle = 72
+
+
+	# bright red
+	fill(255, 0, 0)
+	circle((x_circle, y_circle), 58)
+
+	# text
+
+	text_font(f, 13)
+	fill(0)
+	y_tostring = "Y: " + str(y_circle)
+	x_tostring = "X: " + str(x_circle)
+	text(x_tostring, (x_circle-11, y_circle-11))
+	text(y_tostring, (x_circle-11, y_circle+1))
+
+	if mouse_is_pressed:
+		stroke(255)
+	else:
+		stroke(0)
+
+	line((mouse_x - 66, mouse_y), (mouse_x + 66, mouse_y))
+	line((mouse_x, mouse_y - 66), (mouse_x, mouse_y + 66))
+
+
 	# # dark red
 	# fill(127, 0, 0)
 	# circle((144, 72), 58)
