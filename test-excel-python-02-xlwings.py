1eab5aa58a7646eb62d9e7ca8e624d6000420b32
Zoltan Dul <zoltan.dul@gmail.com>
2021-04-23 15:34:17 -0600

    ℹ️ add xlwings lib test

---
+++ test-excel-python-02-xlwings.py
@@ -0,0 +1,10 @@
+
+import xlwings as xw
+import datetime as dt
+
+
+app = xw.App()
+# sheet.range('A1').value = 1
+# sheet.range('A2').value = 'Hello'
+# sheet.range('A3').value is None
+# sheet.range('A4').value = dt.datetime(2000, 1, 1)
