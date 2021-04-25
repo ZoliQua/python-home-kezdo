e368a6d5c5d7e80fdec09d543a6deee4bc600647
Zoltan Dul <zoltan.dul@gmail.com>
2021-04-25 16:34:57 +0200

    ▶️ update test-excel-python-02-xlwings.py

--- test-excel-python-02-xlwings.py
+++ test-excel-python-02-xlwings.py
@@ -1,13 +1,14 @@
 
 import xlwings as xw
+import pandas as pd
 import datetime as dt
-
 
 app = xw.Book(r"data/Book1.xlsx")
 
 sheet = app.sheets[0]
 # print(sheet.range('A1').value, sheet.range('B1').value, sheet.range('C1').value, sheet.range('D1').value)
 print(sheet.range('A1:C40').value)
+
 # sheet.range('A1').value = 1
 # sheet.range('A2').value = 'Hello'
 # sheet.range('A3').value is None
