75df2ce1c474c35b272d81e1dcbf2e65e2ebc9a0
Zoltán Dul <zoltan.dul@gmail.com>
2021-06-17 19:29:21 +0200

    ➕ Add test-image-recogniton.py

---
+++ test-image-recogniton.py
@@ -0,0 +1,9 @@
+# Look at this: https://github.com/ultralytics/google-images-download
+# And this: https://google-images-download.readthedocs.io/en/latest/installation.html#install-using-pip
+# And this: https://preettheman.medium.com/how-to-scrape-data-from-instagram-with-python-c4158225321d
+# And this: https://medium.com/codex/extracting-text-from-images-cb4d0f59a71b
+
+# IdenProof: https://towardsdatascience.com/train-image-recognition-ai-with-5-lines-of-code-8ed0bdd8d9ba
+# Coda: https://github.com/OlafenwaMoses/IdenProf
+# (downloaded into images)
+# Chrome Driver
