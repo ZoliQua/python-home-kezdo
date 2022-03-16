c21329c172956934098077f5d7ab6f05e0abd4d2
Zoltán Dul <zoltan.dul@gmail.com>
2022-03-16 07:20:31 +0100

    updating test-clustermap-seaborn-extract-data.py

---
+++ test-clustermap-seaborn-extract-data.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+sns.set(font="monospace")
+
+df = sns.load_dataset("brain_networks", header=[0, 1, 2], index_col=0)
+used_networks = [1, 5, 6, 7, 8, 11, 12, 13, 16, 17]
+used_columns = (df.columns.get_level_values("network")
+                          .astype(int)
+                          .isin(used_networks))
+df = df.loc[:, used_columns]
+
+network_pal = sns.cubehelix_palette(len(used_networks),
+                                    light=.9, dark=.1, reverse=True,
+                                    start=1, rot=-2)
+network_lut = dict(zip(map(str, used_networks), network_pal))
+
+networks = df.columns.get_level_values("network")
+network_colors = pd.Series(networks).map(network_lut)
+
+cmap = sns.diverging_palette(h_neg=210, h_pos=350, s=90, l=30, as_cmap=True)
+
+result = sns.clustermap(df.corr(), row_colors=network_colors, method="average",
+               col_colors=network_colors, figsize=(13, 13), cmap=cmap)
+
+from scipy.spatial import distance
+from scipy.cluster import hierarchy
+
+correlations = df.corr()
+correlations_array = np.asarray(df.corr())
+
+row_linkage = hierarchy.linkage(
+    distance.pdist(correlations_array), method='average')
+
+col_linkage = hierarchy.linkage(
+    distance.pdist(correlations_array.T), method='average')
+
+sns.clustermap(correlations, row_linkage=row_linkage, col_linkage=col_linkage, row_colors=network_colors, method="average",
+               col_colors=network_colors, figsize=(13, 13), cmap=cmap)
+
+plt.show()
