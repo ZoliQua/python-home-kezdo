6340138133995866a9b52ec5247d118c9b10fd98
Zoltán Dul <zoltan.dul@gmail.com>
2023-01-13 20:26:31 +0100

    modified

--- otos-lotto-pair2-all.py
+++ otos-lotto-pair2-all.py
@@ -1,12 +1,8 @@
 
+#test file
 
 for i in range(1, 90):
 	start = i + 1
 	for j in range(start, 91):
 		toprint = str(i).zfill(2) + "_" + str(j).zfill(2)
 		print(toprint)
-
-
-		# for k in range(3, 89):
-		# 	for l in range(4, 90):
-		# 		for m in range(5, 91):
